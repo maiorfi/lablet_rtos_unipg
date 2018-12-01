0ce014ae30362a1a2ab0e8683d2c5871f1edfa75
Lorenzo Maiorfi <lorenzo.maiorfi@k-digitale.com>
2018-12-01 09:59:26 +0100

    README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+## STEP #5
+
++ Utilizzo di EventFlag
++ wait_all() e wait_any()
--- main.cpp
+++ main.cpp
@@ -15,22 +15,24 @@ static InterruptIn btnBlack(PA_15);
 
 SWO_Channel swo("channel");
 
-static Timer s_boot_timer;
-
 static Thread s_thread_1;
 static Thread s_thread_2;
 static Thread s_thread_3;
 
+// this is a set of flags used to "orchestrate" threads
 EventFlags completionFlags;
 
+// event flag used in same set have to be "orthogonal"
 #define THREAD_1_COMPLETED (1 << 0)
 #define THREAD_2_COMPLETED (1 << 1)
 #define THREAD_3_COMPLETED (1 << 2)
 
+// let's define an alias for our too-long thread argument type
 using MyThreadsArguments = std::tuple<int, DigitalOut *, int, int, uint8_t>;
 
 static void thread_procedure(MyThreadsArguments *args)
 {
+    // unpack passed tuple
     int threadId = std::get<0>(*args);
     DigitalOut *digitalOut = std::get<1>(*args);
     int delayMs = std::get<2>(*args);
@@ -49,6 +51,7 @@ static void thread_procedure(MyThreadsArguments *args)
 
     swo.printf("[Thread #%d] ...done.\n", threadId);
 
+    // now we signal this thread is done
     completionFlags.set(completionFlag);
 }
 
@@ -56,8 +59,7 @@ int main()
 {
     swo.printf("[MAIN] Starting main()...\n");
 
-    s_boot_timer.start();
-
+    // define 3 different (set of) arguments
     MyThreadsArguments args1(1, &ledGreen, 500, 10, THREAD_1_COMPLETED);
     MyThreadsArguments args2(2, &ledYellow, 200, 20, THREAD_2_COMPLETED);
     MyThreadsArguments args3(3, &ledRed, 300, 30, THREAD_3_COMPLETED);
@@ -66,6 +68,8 @@ int main()
     s_thread_2.start(callback(thread_procedure, &args2));
     s_thread_3.start(callback(thread_procedure, &args3));
 
+    // here main thread is waiting for --all-- threads being completed, without burning CPU cycles
+    // note: try to use wait_any() instead of wait_all()
     completionFlags.wait_all(THREAD_1_COMPLETED | THREAD_2_COMPLETED | THREAD_3_COMPLETED);
 
     swo.printf("[MAIN] ...exiting from main()\n");
