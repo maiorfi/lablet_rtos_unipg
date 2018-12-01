dfaa5298e8b07e8f592587e1b78294035a740e11
Lorenzo Maiorfi <lorenzo.maiorfi@k-digitale.com>
2018-12-01 12:54:30 +0100

    Completamento esercizio

--- main.cpp
+++ main.cpp
@@ -20,9 +20,13 @@ static Thread s_thread_writer;
 
 static Timer s_main_timer;
 
+// Coda condivisa tra thread reader e writer
 static std::deque<int> s_intervals_queue;
+
+// Mutex per proteggere dall'accesso concorrente (potenzialmente distruttivo!) 
 static Mutex s_intervals_queue_lock;
 
+// READER : fa polling sullo stato di un ingresso e accoda le durate degli intervalli di attivazione
 static void reader_thread_procedure()
 {
     s_main_timer.start();
@@ -33,25 +37,29 @@ static void reader_thread_procedure()
 
     while (true)
     {
+        // c'è stata una variazione di stato nell'ingresso?
         bool current_input_state = !btnBlack.read();
 
         if (current_input_state != latest_input_state)
         {
             latest_input_state = current_input_state;
 
+            // è un fronte di salita?
             if (current_input_state)
             {
+                // ...memorizzo il timestamp del fronte
                 latest_rising_timestamp = s_main_timer.read_ms();
             }
             else
             {
+                // ...è un fronte di discesa, calcolo la lunghezza dell'intervallo
                 int interval = s_main_timer.read_ms() - latest_rising_timestamp;
 
+                // quelli troppo brevi li scarto (glitch?)
                 if (interval < 10)
                     continue;
 
-                swo.printf("[READER] Pulse: %d ms\n", interval);
-
+                // intervallo di più di 3 secondi fa uscire dalla funzione
                 if (interval > 3000)
                 {
                     return;
@@ -60,8 +68,10 @@ static void reader_thread_procedure()
                 {
                     s_intervals_queue_lock.lock();
 
+                    // aggiungo l'item alla coda
                     s_intervals_queue.push_back(interval);
-                    swo.printf("[READER] now there are %d item%s in queue\n", s_intervals_queue.size(), (s_intervals_queue.size() > 1) ? "s" : "");
+                    int size=s_intervals_queue.size();
+                    swo.printf("[READER] added item %d (%d item%s in queue)\n", interval, size, size > 1 ? "s" : "");
 
                     s_intervals_queue_lock.unlock();
                 }
@@ -70,9 +80,11 @@ static void reader_thread_procedure()
     }
 }
 
+// WRITER : scoda gli item (FIFO) e attiva le uscite di conseguenza, "eseguendo" un pattern applicativo
 static void writer_thread_procedure()
 {
     int item;
+    int size;
 
     while (true)
     {
@@ -80,18 +92,23 @@ static void writer_thread_procedure()
 
         s_intervals_queue_lock.lock();
 
+        // memorizzo su variabili locali i dati della coda che mi interessano
+        // nota: questo "caching" mi consente di tenere la coda lockata il meno possibile, massimizzando l'effettivo parallelismo
         if (!s_intervals_queue.empty())
         {
             item = s_intervals_queue.front();
             s_intervals_queue.pop_front();
+            size=s_intervals_queue.size();
         }
 
         s_intervals_queue_lock.unlock();
 
+        // c'è almeno un elemento nella coda?
         if (item!=-1)
         {
-            swo.printf("[WRITER] dequeued item %d\n", item);
+            swo.printf("[WRITER] dequeued item %d (%d in queue)\n", item, size);
             
+            // decodifico l'item (lunghezza intervallo->selezione uscita da attivare)
             if (item < 500)
             {
                 ledGreen.write(true);
@@ -124,6 +141,7 @@ int main()
     s_thread_reader.start(reader_thread_procedure);
     s_thread_writer.start(writer_thread_procedure);
 
+    // attendo l'uscita del thread "reader"
     s_thread_reader.join();
 
     swo.printf("[MAIN] ...exiting from main()\n");
