bf9ba4649f87bcdd5b8ddee30a0d0cf2ec30b2d9
Lorenzo Maiorfi <lorenzo.maiorfi@k-digitale.com>
2018-12-01 09:53:34 +0100

    README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+## STEP #4
+
++ Creazione di più Thread
++ Utilizzo di una stessa "thread procedure" da parte di più Thread
++ Utilizzo della classe (template) std::tuple ([Info](https://en.cppreference.com/w/cpp/utility/tuple))
--- main.cpp
+++ main.cpp
@@ -7,33 +7,38 @@
 static DigitalOut ledOnBoard(LED1);
 static InterruptIn btnOnBoard(BUTTON1);
 
+// let's use 3 leds to detect threads activity
 static DigitalOut ledRed(PD_0, true);
 static DigitalOut ledYellow(PD_1, true);
 static DigitalOut ledGreen(PG_0, true);
 
+// we're not going to use this right now
 static InterruptIn btnBlack(PA_15);
 
 SWO_Channel swo("channel");
 
-static Timer s_boot_timer;
-
+// here we have 3 Thread instances
 static Thread s_thread_1;
 static Thread s_thread_2;
 static Thread s_thread_3;
 
+// in this case, the same function is run by all three threads we're going to start
+// note:    a std::tuple is a (template) class defined in standard c++ library
+//          its purpose is that of containing ordered objects of different types
 static void thread_procedure(std::tuple<DigitalOut *, int> *args)
 {
+    // extract first tuple variable (0-based)
     DigitalOut *digitalOut = std::get<0>(*args);
+    
+    // extract second tuple variable
     int delayMs = std::get<1>(*args);
-
-    int state = 0;
 
     while (true)
     {
         wait_ms(delayMs);
-        digitalOut->write(state);
 
-        state = (state ? 0 : 1);
+        // just toggle led
+        digitalOut->write(!digitalOut->read());
     }
 }
 
@@ -41,16 +46,17 @@ int main()
 {
     swo.printf("[MAIN] Starting main()...\n");
 
-    s_boot_timer.start();
-
+    // a different argument-set passed to each of 3 threads (3 leds, with 3 different toggling interval)
     std::tuple<DigitalOut *, int> args1(&ledGreen, 500);
     std::tuple<DigitalOut *, int> args2(&ledYellow, 200);
     std::tuple<DigitalOut *, int> args3(&ledRed, 300);
 
+    // let's start the threads
     s_thread_1.start(callback(thread_procedure, &args1));
     s_thread_2.start(callback(thread_procedure, &args2));
     s_thread_3.start(callback(thread_procedure, &args3));
 
+    // wait here forever (BEWARE: this is going to "burn" CPU cycles for no reason!)
     while(true) {}
 
     swo.printf("[MAIN] ...exiting from main()\n");
