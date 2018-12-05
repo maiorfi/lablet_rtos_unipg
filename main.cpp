1226c43271f7cbb2683912ee04a46758959b06e8
Lorenzo Maiorfi <lorenzo.maiorfi@k-digitale.com>
2018-12-05 12:34:28 +0100

    Sample esercitazione 1

--- main.cpp
+++ main.cpp
@@ -2,24 +2,52 @@
 
 #include "SWO.h"
 
-static DigitalOut ledOnBoard(LED1);
+static DigitalOut led1(LED1);
+static DigitalOut led2(LED2);
+static DigitalOut led3(LED3);
+
+static DigitalIn btn(USER_BUTTON);
 
 SWO_Channel swo("channel");
 
-static Timer s_boot_timer;
-
-// A Thread object. It will be started later, in main()
 static Thread s_thread_1;
+static Thread s_thread_2;
+
+static EventFlags s_event_flags;
+
+#define EVENT_FLAG_ID_1 (0x01)
 
 // This is the (never-ending, in this case) function run by "s_thread_1"
-static void thread_procedure()
+static void thread_procedure_1()
 {
     while(true)
     {
-        wait(0.5);
+        if(s_event_flags.get() & EVENT_FLAG_ID_1)
+        {
+            // just toggle on-board led
+            led1.write(!led1.read());
 
-        // just toggle on-board led
-        ledOnBoard.write(!ledOnBoard.read());
+            wait(0.5);
+        }
+        else
+        {
+            led1.write(false);
+        }
+    }
+}
+
+static void thread_procedure_2()
+{
+    while(true)
+    {
+        if(btn.read())
+        {
+            s_event_flags.set(EVENT_FLAG_ID_1);
+        }
+        else
+        {
+            s_event_flags.clear(EVENT_FLAG_ID_1);
+        }
     }
 }
 
@@ -27,10 +55,8 @@ int main()
 {
     swo.printf("[MAIN] Starting main()...\n");
 
-    s_boot_timer.start();
-
-    // let's start a thread, passing function to be executed
-    s_thread_1.start(thread_procedure);
+    s_thread_1.start(thread_procedure_1);
+    s_thread_2.start(thread_procedure_2);
 
     // at this point we have 2 threads running: the one running main() and the one running thread_procedure()
     swo.printf("[MAIN] ...exiting from main()\n");
